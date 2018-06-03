d4e13345288b1daa468597010dbfd116bbb2348f
Yasuhisa Yoshida <syou6162@gmail.com>
2018-06-03 13:29:20 +0900

    SPAでもgoogle analiticsがきちんと動くようにする

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
+import VueAnalytics from 'vue-analytics'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -11,6 +12,11 @@ import routes from './routes'
 const router = new VueRouter({
   routes: routes
 });
+
+Vue.use(VueAnalytics, {
+  id: 'UA-591180-8',
+  router
+})
 
 const app = new Vue({
   el: '#app',
