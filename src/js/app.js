5f7f860cc46f2a0c800411cc161579937f5210cb
Yasuhisa Yoshida <syou6162@gmail.com>
2018-10-20 15:24:15 +0900

    件数が多いと画像のロードが時間かかるのでlazy-loadする

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import VueAnalytics from 'vue-analytics'
+import VueLazyload from 'vue-lazyload'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueRouter)
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue)
+Vue.use(VueLazyload)
 
 import routes from './routes'
 const router = new VueRouter({
