b51f117f9d555fb6dba079bb3c40cea2d5f02be1
Yasuhisa Yoshida <syou6162@gmail.com>
2018-10-20 18:28:20 +0900

    api側のテストも追加

--- lib/web/web_test.go
+++ lib/web/web_test.go
@@ -9,6 +9,10 @@ import (
 	"log"
 	"os"
 
+	"net/url"
+	"strings"
+
+	"github.com/syou6162/go-active-learning-web/lib/search"
 	"github.com/syou6162/go-active-learning-web/lib/web"
 	"github.com/syou6162/go-active-learning/lib/cache"
 	"github.com/syou6162/go-active-learning/lib/db"
@@ -109,6 +113,51 @@ func TestGetExamplesFromList(t *testing.T) {
 	}
 	w := httptest.NewRecorder()
 	http.HandlerFunc(web.GetExamplesFromList).ServeHTTP(w, req)
+
+	if status := w.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	examples := example.Examples{}
+	json.Unmarshal(w.Body.Bytes(), &examples)
+
+	if len(examples) == 0 {
+		t.Error("Result must not be empty")
+	}
+}
+
+func TestSearch(t *testing.T) {
+	inputFilename := "../../tech_input_example.txt"
+	train, err := file.ReadExamples(inputFilename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	cache.AttachMetadata(train, true, true)
+
+	for _, example := range train {
+		_, err = db.InsertOrUpdateExample(example)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	if err = search.Init(); err != nil {
+		t.Error(err)
+	}
+	defer search.Close()
+
+	values := url.Values{}
+	values.Set("query", "blog")
+
+	req, err := http.NewRequest("POST", "/api/search", strings.NewReader(values.Encode()))
+	if err != nil {
+		t.Error(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w := httptest.NewRecorder()
+	http.HandlerFunc(web.Search).ServeHTTP(w, req)
 
 	if status := w.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
