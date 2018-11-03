c92896469aa55202f884ef9fa2b7d1be1d37866b
Yasuhisa Yoshida <syou6162@gmail.com>
2018-11-04 02:18:25 +0900

    ボタンがはみださないように

--- src/js/Example.vue
+++ src/js/Example.vue
@@ -3,9 +3,9 @@
     <b-card no-body>
       <b-card-body v-bind:title="example | getTitle(75, '...')" title-tag="h5" class="m-1 p-2">
         <b-card-footer>
+          <b-button v-bind:href="'/example/' + encodeURIComponent(example.FinalUrl)" class="float-right" size="sm">Read more</b-button>
           <img v-if="example.Favicon" style="width: 16px; height: 16px;" v-lazy="example.Favicon" onerror="this.style.display='none'" />
           <a v-bind:href="example.FinalUrl">{{ example | getDomain }} {{ example | getUserName }}</a>
-          <b-button v-bind:href="'/example/' + encodeURIComponent(example.FinalUrl)" class="float-right" size="sm">Read more</b-button>
         </b-card-footer>
       </b-card-body>
     </b-card>
