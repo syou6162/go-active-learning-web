6ad2bc0b0e9195995a05e89c4ce1b58092992392
Yasuhisa Yoshida <syou6162@gmail.com>
2018-11-11 17:40:38 +0900

    responseをjsonでいい感じに返せるように

---
+++ lib/web/response.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, status int, v interface{}) {
+	res, err := json.Marshal(v)
+	if err != nil {
+		ServerError(w, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(res)
+}
+
+func Ok(w http.ResponseWriter, msg string) {
+	var data struct {
+		Msg string `json:"message"`
+	}
+	data.Msg = msg
+	JSON(w, http.StatusOK, data)
+}
+
+func BadRequest(w http.ResponseWriter, msg string) {
+	var data struct {
+		Error string `json:"error"`
+	}
+	data.Error = msg
+	JSON(w, http.StatusBadRequest, data)
+}
+
+func ServerError(w http.ResponseWriter, msg string) {
+	var data struct {
+		Error string `json:"error"`
+	}
+	data.Error = msg
+	JSON(w, http.StatusInternalServerError, data)
+}
+
+func UnavaliableError(w http.ResponseWriter, msg string) {
+	var data struct {
+		Error string `json:"error"`
+	}
+	data.Error = msg
+	JSON(w, http.StatusServiceUnavailable, data)
+}
