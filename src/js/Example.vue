86cf3cfe63b47640869b2274666e62136b72a61f
Yasuhisa Yoshida <syou6162@gmail.com>
2018-11-17 22:50:30 +0900

    プラベートユーザーもいるのでcountのほうが正確

--- src/js/Example.vue
+++ src/js/Example.vue
@@ -2,8 +2,8 @@
   <div>
     <b-card no-body>
       <b-card-body v-bind:title="example | getTitle(75, '...')" title-tag="h5" class="m-1 p-2">
-        <div v-if="example.HatenaBookmark.bookmarks.length > 0">
-          <a v-bind:href="example.HatenaBookmark.entry_url" style="color: #ff4166;">{{ example.HatenaBookmark.bookmarks.length}} users</a>:
+        <div v-if="example.HatenaBookmark.count > 0">
+          <a v-bind:href="example.HatenaBookmark.entry_url" style="color: #ff4166;">{{ example.HatenaBookmark.count }} users</a>:
           <hatena-bookmark-icon 
             v-for="b in example.HatenaBookmark.bookmarks.slice(0, 9)"
             v-bind:bookmark="b"
