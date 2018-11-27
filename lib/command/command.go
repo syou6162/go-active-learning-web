4a64e1e47659cd8dd87adef1f78497aa69a92458
Yasuhisa Yoshida <syou6162@gmail.com>
2018-11-28 01:14:24 +0900

    addコマンドは共通のところに移動

--- lib/command/command.go
+++ lib/command/command.go
@@ -2,10 +2,10 @@ package command
 
 import (
 	"github.com/codegangsta/cli"
-	"github.com/syou6162/go-active-learning-web/lib/add"
 	"github.com/syou6162/go-active-learning-web/lib/recommend"
 	"github.com/syou6162/go-active-learning-web/lib/twitter"
 	"github.com/syou6162/go-active-learning-web/lib/web"
+	"github.com/syou6162/go-active-learning/lib/add"
 )
 
 var Commands = []cli.Command{
