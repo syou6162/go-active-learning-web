640e8cb25e379769a0321d7d2b1c9f6aa20c0c76
Yasuhisa Yoshida <syou6162@gmail.com>
2018-12-17 21:13:32 +0900

    位置の変更

--- src/js/Example.vue
+++ src/js/Example.vue
@@ -18,15 +18,15 @@
              v-bind:key="tweet.ScreenName"
              ></twitter-icon>
         </div>
+        <annotate-buttons
+          v-if="isAdmin"
+          v-bind:example="example"
+          ></annotate-buttons>
         <b-card-footer>
           <b-button v-bind:href="'/example/' + encodeURIComponent(example.FinalUrl)" class="float-right" size="sm" v-bind:variant="example | getButtonStyle">Read more</b-button>
           <img v-if="example.Favicon" style="width: 16px; height: 16px;" v-lazy="example.Favicon" onerror="this.style.display='none'" />
           <a v-bind:href="example.FinalUrl">{{ example | getDomain }} {{ example | getUserName }}</a>
         </b-card-footer>
-        <annotate-buttons
-          v-if="isAdmin"
-          v-bind:example="example"
-          ></annotate-buttons>
       </b-card-body>
     </b-card>
   </div>
