a3925fe23b444b8917a2544ab2fd29c7252a6f94
Yasuhisa Yoshida <syou6162@gmail.com>
2018-12-17 16:01:49 +0900

    amplify追加

--- src/js/util.js
+++ src/js/util.js
@@ -34,3 +34,38 @@ export function filterBookmarksWithComment(example) {
     return b.comment !== "";
   });
 }
+
+import Amplify, {
+  Auth,
+  API,
+} from 'aws-amplify';
+
+Amplify.Logger.LOG_LEVEL = 'DEBUG';
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    identityPoolId: 'us-east-1:a61e4d83-cd2a-4bd3-8995-19f9e2726f76',
+    userPoolId: 'us-east-1_4Jft7xFAo',
+    userPoolWebClientId: '4ia5ifrn456rqg4vr6dqfh7e68',
+  },
+});
+
+export function signUp(username, password) {
+  return Auth.signUp({
+    username,
+    password,
+  });
+}
+
+export function signIn(username, password) {
+  return Auth.signIn(username, password);
+}
+
+export function signOut() {
+  return Auth.signOut();
+}
+
+export function currentSession() {
+  return Auth.currentSession();
+}
+
