eac6bd103b8aaf36eecb4b7a733761cfd83ebd5c
Yasuhisa Yoshida <syou6162@gmail.com>
2018-12-31 20:56:54 +0900

    事例詳細のページを追加

---
+++ pages/example/_ExampleDetail.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="mx-auto" style="max-width: 40rem;">
+    <b-card v-if="example" v-bind:title="example | getTitle(1000, '...')" tag="article">
+      <img v-if="example.OgImage" class="img-thumbnail img-responsive" style="width: 128px; height: 96px; margin: 3px; float: right;" v-lazy="example.OgImage" onerror="this.style.display='none'" />
+      <p class="card-text">
+        {{ example | getDescription(1000, '...') }}
+      </p>
+      <div v-if="example.HatenaBookmark.count > 0">
+        <a v-bind:href="example.HatenaBookmark.entry_url" style="color: #ff4166;">{{ example.HatenaBookmark.count }} users</a>:
+        <hatena-bookmark-icon 
+          v-for="b in example.HatenaBookmark.bookmarks.slice(0, 9)"
+          v-bind:bookmark="b"
+          v-bind:key="b.user"
+          ></hatena-bookmark-icon>
+      </div>
+      <div v-if="example.ReferringTweets && example.ReferringTweets.length > 0">
+        <span style="color: #ff4166;">{{ example.ReferringTweets.length }} mentions</span>:
+        <twitter-icon
+           v-for="tweet in example.ReferringTweets.slice(0, 8)"
+           v-bind:tweet="tweet"
+           v-bind:key="tweet.ScreenName"
+           ></twitter-icon>
+      </div>
+      <div v-if="keywords.length > 0">
+        Keywords: 
+        <b-link 
+          v-bind:href="'/search?query=' + k" 
+          v-for="k in keywords"
+          v-bind:key="k"
+          >
+          {{ k }}
+        </b-link>
+      </div>
+      Date: {{ example.CreatedAt }}
+      <annotate-buttons
+        v-if="isAdmin"
+        v-bind:example="example"
+        ></annotate-buttons>
+      <b-card-footer>
+        <img v-if="example.Favicon" style="width: 16px; height: 16px;" v-lazy="example.Favicon" onerror="this.style.display='none'" />
+        <a v-bind:href="example.FinalUrl">{{ example | getDomain }} {{ example | getUserName }}</a>
+      </b-card-footer>
+    </b-card>
+    <div v-if="example.ReferringTweets && example.ReferringTweets.length > 0">
+      <h4>Referring Tweets</h4>
+      <b-list-group>
+        <b-list-group-item v-for="t in example.ReferringTweets.slice(0, 3)" :key="t.ScreenName">
+          <img v-if="t.ProfileImageUrl" style="width: 24px; height: 24px;" v-lazy="t.ProfileImageUrl" onerror="this.style.display='none'" />
+          <a v-bind:href="'https://twitter.com/' + t.ScreenName + '/status/' + t.IdStr">@{{ t.ScreenName }}</a>
+          {{ t.FullText }}
+        </b-list-group-item>
+      </b-list-group>
+    </div>
+    <div v-if="hasBookmarksWithComment">
+      <h4>Bookmark Comments</h4>
+      <b-list-group>
+        <b-list-group-item v-for="b in bookmarksWithComment.slice(0, 3)" :key="b.user">
+          <img style="width: 24px; height: 24px; margin: 2px" v-lazy="'https://cdn.profile-image.st-hatena.com/users/' + b.user+ '/profile.png'" />
+          <a v-bind:href="'http://b.hatena.ne.jp/' + b.user">id:{{ b.user }}</a>
+          {{ b.comment }}
+        </b-list-group-item>
+      </b-list-group>
+    </div>
+    <h4 v-if="similarExamples.length > 0">Related Entries</h4>
+    <b-list-group>
+      <b-list-group-item v-for="example in similarExamples" :key="example.FinalUrl">
+        <b-button v-bind:href="example | getEncodedUrl" class="float-right" size="sm">Read more</b-button>
+        <img v-if="example.Favicon" style="width: 16px; height: 16px;" v-lazy="example.Favicon" onerror="this.style.display='none'" />
+        {{ example | getTitle(100, '...') }}
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { Auth } from 'aws-amplify';
+import Example from '~/components/Example.vue';
+import HatenaBookmarkIcon from '~/components/HatenaBookmarkIcon.vue';
+import TwitterIcon from '~/components/TwitterIcon.vue';
+import AnnotateButtons from '~/components/AnnotateButtons.vue';
+import { NewExample, filterBookmarksWithComment } from '~/assets/util';
+
+export default {
+  data () {
+    return {
+      title: "ML News",
+      url: this.$route.params.url,
+      example: null,
+      similarExamples: [],
+      keywords: [],
+      isAdmin: false,
+    }
+  },
+  mounted() {
+    Auth.currentAuthenticatedUser()
+      .then(user => {
+        this.isAdmin = true;
+      })
+      .catch(err => console.log(err))
+  },
+  async asyncData(context) {
+    console.log(context.route);
+    const url = context.route.params.ExampleDetail;
+    let data = await context.app.$axios.$get("/api/example?url=" + encodeURIComponent(url));
+    return {
+      title: `ML News - ${data.Example.Title}`,
+      example: NewExample(data.Example),
+      similarExamples: data.SimilarExamples.filter(function(e) {
+        return e.Label === 1 || e.Score > 0.0;
+      }),
+      keywords: data.Keywords,
+    }
+  },
+  head() {
+    const tweets = this.example.ReferringTweets.map(t => "@" + t.ScreenName + "「" + t.FullText.substr(0, 100) + "...」").slice(0, 3);
+    const bookmarks = filterBookmarksWithComment(this.example).map(b => "id:" + b.user + "「"+ b.comment + "」").slice(0, 3);
+    const description = tweets.join("\n") + bookmarks.join("\n");
+
+    return {
+      title: this.title,
+      meta: [
+        {
+          name: "keywords",
+          content: this.keywords.join(",")
+        },
+        {
+          name: "description",
+          content: description
+        }
+      ],
+      link: [
+        {
+          rel: "canonical",
+          href: `https://www.machine-learning.news/example/${encodeURIComponent(this.example.Url)}`
+        }
+      ]
+    };
+  },
+  computed: {
+    hasBookmarksWithComment() {
+      return filterBookmarksWithComment(this.example).length > 0;
+    },
+    bookmarksWithComment() {
+      return filterBookmarksWithComment(this.example);
+    },
+  },
+  components: {
+    "hatena-bookmark-icon": HatenaBookmarkIcon,
+    "twitter-icon": TwitterIcon,
+    "annotate-buttons": AnnotateButtons 
+  }
+}
+</script>
