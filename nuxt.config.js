71b42ffd8261a1f14363805ccf7658dde0bd8b71
Yasuhisa Yoshida <syou6162@gmail.com>
2019-01-02 13:43:29 +0900

    情報が重複するので消す

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,6 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
