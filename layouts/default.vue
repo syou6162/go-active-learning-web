740dca51591ea3c0804471e46208baa6c7cfa837
Yasuhisa Yoshida <syou6162@gmail.com>
2019-01-02 20:18:35 +0900

    footerに情報を載せておく

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,5 +16,9 @@
       </b-collapse>
     </b-navbar>
     <nuxt/>
+    <div class="footer" style="text-align: center; color: #8c8c8c;">
+      <span>ML-News関連リンク: <a href="https://twitter.com/syou6162">開発者Twitter</a>, <a href="https://github.com/syou6162/go-active-learning-web">Github</a></span><br />
+      <span>ML-Newsはユーザビリティの改善や分析のためGoogle Analyticsを使用しています</span>
+    </div>
   </div>
 </template>
