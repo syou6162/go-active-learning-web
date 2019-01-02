153f5d00ca8fd73a404787bf30d6c56811b9756f
Yasuhisa Yoshida <syou6162@gmail.com>
2019-01-02 20:08:38 +0900

    og:type追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { property: 'og:type', content: 'website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
