63f8be89a298466cd07d94104e57d113d466517c
Yasuhisa Yoshida <syou6162@gmail.com>
2019-01-09 00:40:22 +0900

    ユーザーに見せる必要はない

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,7 +11,6 @@
           <b-nav-item to="/list/arxiv">arXiv</b-nav-item>
           <b-nav-item to="/list/slide">Slide</b-nav-item>
           <b-nav-item to="/search">Search</b-nav-item>
-          <b-nav-item to="/recent-added-examples">recent-added-examples</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
