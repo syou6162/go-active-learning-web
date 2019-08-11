79e2c1e3dcd02858d782f7273b36d8732dfbe938
Yasuhisa Yoshida <syou6162@gmail.com>
2019-08-11 23:45:14 +0900

    アノテーションボタンのTypeScript化

--- components/AnnotateButtons.vue
+++ components/AnnotateButtons.vue
@@ -7,28 +7,31 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
 import axios from 'axios';
+import Example from '~/models/Example'
 
-export default {
-  props: ['example'],
-  methods: {
-    updateLabel(example, label) {
-      let idToken = localStorage.getItem("CognitoIdentityServiceProvider.4ia5ifrn456rqg4vr6dqfh7e68.yasuhisa.idToken");
-      let headers = { headers: { 'Authorization': idToken } };
-      axios.post(
-        "https://3ojd2wnlpg.execute-api.us-east-1.amazonaws.com/Prod/update_example_label", 
-        {
-          id: example.Id,
-          label: label,
-        },
-        headers
-      ).then(response => {
-        example.Label = response.data.Label;
-      }).catch(function (error) {
-        alert(`${error}: Failed to annotate "${example.Title}"`);
-      })
-    },
+@Component
+export default class AnnotateButtons extends Vue {
+  @Prop({required: true})
+  example!: Object
+  
+  updateLabel(example: Example, label: number) {
+    let idToken = localStorage.getItem("CognitoIdentityServiceProvider.4ia5ifrn456rqg4vr6dqfh7e68.yasuhisa.idToken");
+    let headers = { headers: { 'Authorization': idToken } };
+    axios.post(
+      "https://3ojd2wnlpg.execute-api.us-east-1.amazonaws.com/Prod/update_example_label", 
+      {
+        id: example.Id,
+        label: label,
+      },
+      headers
+    ).then(response => {
+      example.Label = response.data.Label;
+    }).catch(function (error) {
+      alert(`${error}: Failed to annotate "${example.Title}"`);
+    })
   }
 }
 </script>
