c393e23e3146684702cbe4d9f66a453c1d3a307a
Yasuhisa Yoshida <syou6162@gmail.com>
2019-11-17 02:44:03 +0900

    キーワードの順位付けをした

--- lib/ahocorasick/ahocorasick.go
+++ lib/ahocorasick/ahocorasick.go
@@ -8,154 +8,33 @@ import (
 )
 
 var (
-	once    sync.Once
-	machine *goahocorasick.Machine
+	once                  sync.Once
+	machinePriorityHigh   *goahocorasick.Machine
+	machinePriorityMedium *goahocorasick.Machine
+	machinePriorityLow    *goahocorasick.Machine
 )
 
-var keywords = []string{
-	"機械学習",
-	"machine learning",
-	"deep learning",
-	"深層学習",
-	"end-to-end",
-	"データサイエンス",
-	"データサイエンティスト",
-	"データエンジニアリング",
-
-	"強化学習",
-	"深層強化学習",
-	"reinforcement learning",
-	"ベルマン方程式",
+var keywordsPriorityHigh = []string{
 	"active learning",
-	"neural net",
-	"neural network",
-	"ニューラルネット",
-	"ディープラーニング",
-	"tpu",
-	"gpu",
 
-	"自然言語処理",
-	"言語処理",
-	"natural language processing",
-	"nlp",
-
-	"言語モデル",
-	"language model",
-	"language generation",
-	"text generation",
-
-	"機械翻訳",
-	"machine translation",
-
-	"要約",
-	"summarization",
-	"attention",
-
-	"parsing",
-
-	"structure learning",
-	"structured learning",
-	"構造学習",
-
-	"固有表現抽出",
-	"固有表現認識",
-	"seq2seq",
-	"conditional random field",
-	"named entity recognition",
-	"relation extraction",
-	"関係抽出",
-	"系列ラベリング",
-	"distant supervision",
-	"情報検索",
-	"information retrieval",
-	"画像検索",
-	"質問応答",
-	"question answering",
-
-	"テキストマイニング",
-	"text mining",
 	"差分プライバシー",
 	"differential privacy",
-	"暗号化",
-	"anonymization",
-	"匿名化",
-
-	"対話",
-	"dialogue",
-	"dialog",
-	"マルチモーダル",
-	"multimodal",
 
 	"アノテーション",
 	"annotation",
 
-	"コンピュータビジョン",
-	"computer vision",
-	"画像生成",
-	"image generation",
-	"fashion",
-
-	"信号処理",
-	"音声認識",
-	"音声合成",
-
-	"異常検知",
-	"anomaly detection",
+	"変化検知",
+	"change point detection",
 	"外れ値検出",
 	"outlier detection",
 	"特異スペクトル変換法",
 
-	"変化検知",
-	"change point detection",
-
-	"community detection",
-
-	"metric learning",
-	"距離学習",
-	"双曲空間",
-
-	"物体検出",
-	"semantic segmentation",
-	"segmentation",
-	"セグメンテーション",
 	"姿勢推定",
 	"yolo",
 
-	"cifar",
-	"imagenet",
-
-	"レコメンド",
-	"レコメンデーション",
-	"recommender",
-	"recommendation",
-	"協調フィルタリング",
-	"factorization machine",
-	"collaborative filtering",
-	"learning to rank",
-	"ランキング学習",
 	"lambdamart",
 	"approximate nearest neighbor",
 
-	"bandit",
-
-	"時系列",
-	"time series",
-
-	"医療",
-	"medical",
-	"fairness",
-
-	"線形代数",
-	"linear algebra",
-
-	"データ基盤",
-	"データ分析",
-	"データウェアハウス",
-	"データレイク",
-	"data lake",
-	"data warehouse",
-	"データマート",
-	"data mart",
 	"redash",
 	"re:dash",
 	"data studio",
@@ -170,7 +49,259 @@ var keywords = []string{
 	"airflow",
 	"argo",
 	"データパイプライン",
+	"fisher information",
 
+	"hivemall",
+	"dask",
+	"nnabla",
+	"edward",
+
+	"ngboost",
+	"sentencepiece",
+	"openpose",
+	"scdv",
+	"tslearn",
+	"econml",
+	"deepgbm",
+	"sudachi",
+	"ncrfpp",
+	"hsic",
+	"irgan",
+	"dialogpt",
+	"tinyvideonet",
+	"blazeface",
+	"singan",
+
+	"cookiecutter",
+	"datarobot",
+	"dataflow",
+	"mlflow",
+}
+
+var keywordsPriorityMedium = []string{
+	"深層強化学習",
+	"reinforcement learning",
+
+	"言語モデル",
+	"language model",
+	"language generation",
+	"text generation",
+
+	"要約",
+	"summarization",
+	"attention",
+
+	"構文解析",
+	"parsing",
+
+	"structure learning",
+	"structured learning",
+	"構造学習",
+
+	"固有表現抽出",
+	"固有表現認識",
+	"conditional random field",
+	"named entity recognition",
+	"relation extraction",
+	"関係抽出",
+	"系列ラベリング",
+	"distant supervision",
+	"情報検索",
+	"information retrieval",
+	"画像検索",
+	"質問応答",
+	"question answering",
+
+	"対話",
+	"dialogue",
+	"dialog",
+	"マルチモーダル",
+	"multimodal",
+
+	"異常検知",
+	"anomaly detection",
+
+	"metric learning",
+	"距離学習",
+	"双曲空間",
+
+	"learning to rank",
+	"ランキング学習",
+
+	"bandit",
+	"時系列",
+	"time series",
+
+	"医療",
+	"medical",
+	"fairness",
+
+	"データウェアハウス",
+	"データレイク",
+	"data lake",
+	"data warehouse",
+	"データマート",
+	"data mart",
+
+	"conll",
+	"semeval",
+
+	"recsys",
+	"icdm",
+	"wsdm",
+
+	"factorization machine",
+	"matrix factorization",
+
+	"変分ベイズ",
+
+	"ベイズ最適化",
+	"bayesian optimization",
+	"凸最適化",
+	"convex optimization",
+
+	"転移学習",
+	"transfer learning",
+	"ドメイン適用",
+	"domain adaptation",
+	"data augmentation",
+
+	"tensorboard",
+	"allennlp",
+	"sagemaker",
+	"colab",
+	"scikit-learn",
+	"sklearn",
+	"pandas",
+	"scipy",
+	"julia",
+
+	"lightgbm",
+	"xgboost",
+	"lime",
+	"shap",
+	"fairseq",
+	"tensor2tensor",
+	"stacknet",
+	"fsgan",
+	"deepfake",
+	"molgan",
+	"scibert",
+	"fastspeech",
+	"blazeface",
+	"mecab",
+	"neo4j",
+	"leam",
+	"spacy",
+	"flair",
+	"continuous delivery",
+
+	"albert",
+	"elmo",
+	"electra",
+	"u-net",
+	"xlnet",
+	"stylegan",
+	"octave",
+	"gcn",
+	"cyclegan",
+	"mobilenet",
+	"noise2void",
+	"snorkel",
+	"yolact",
+	"kelpnet",
+
+	"kubeflow",
+	"kafka",
+	"kinesis",
+	"elasticsearch",
+	"amazon forecast",
+	"splunk",
+	"mlops",
+	"automl",
+	"機械学習工学",
+	"reproducibility",
+
+	"kaggle days",
+	"kaggle days tokyo",
+
+	"abテスト",
+	"検定",
+	"statistics",
+	"統計学",
+
+	"音声変換",
+	"link prediction",
+}
+
+var keywordsPriorityLow = []string{
+	"機械学習",
+	"machine learning",
+	"deep learning",
+	"深層学習",
+	"end-to-end",
+	"データサイエンス",
+	"データサイエンティスト",
+	"データエンジニアリング",
+
+	"強化学習",
+	"ベルマン方程式",
+	"neural net",
+	"neural network",
+	"ニューラルネット",
+	"ディープラーニング",
+	"tpu",
+	"gpu",
+
+	"自然言語処理",
+	"言語処理",
+	"natural language processing",
+	"nlp",
+
+	"機械翻訳",
+	"machine translation",
+
+	"seq2seq",
+
+	"テキストマイニング",
+	"text mining",
+	"暗号化",
+	"anonymization",
+	"匿名化",
+
+	"コンピュータビジョン",
+	"computer vision",
+	"画像生成",
+	"image generation",
+	"fashion",
+
+	"信号処理",
+	"音声認識",
+	"音声合成",
+
+	"community detection",
+
+	"物体検出",
+	"semantic segmentation",
+	"segmentation",
+	"セグメンテーション",
+
+	"cifar",
+	"imagenet",
+
+	"レコメンド",
+	"レコメンデーション",
+	"recommender",
+	"recommendation",
+	"協調フィルタリング",
+	"factorization machine",
+	"collaborative filtering",
+
+	"線形代数",
+	"linear algebra",
+
+	"データ基盤",
+	"データ分析",
 	"データセット",
 	"dataset",
 	"コーパス",
@@ -186,20 +317,15 @@ var keywords = []string{
 	"emnlp",
 	"naacl",
 	"coling",
-	"conll",
-	"semeval",
 
 	"nips",
 	"neurips",
 	"cvpr",
 	"icml",
-	"recsys",
-	"icdm",
 	"kdd",
 	"iclr",
 	"ijcai",
 	"aaai",
-	"wsdm",
 	"siggraph",
 	"icra",
 	"eccv",
@@ -216,8 +342,6 @@ var keywords = []string{
 	"gaussian process",
 	"svm",
 
-	"factorization machine",
-	"matrix factorization",
 	"行列分解",
 	"pca",
 	"svd",
@@ -231,21 +355,9 @@ var keywords = []string{
 
 	"ベイズ",
 	"bayesian",
-	"変分ベイズ",
 	"生成モデル",
-	"fisher information",
-
-	"ベイズ最適化",
-	"bayesian optimization",
-	"凸最適化",
-	"convex optimization",
 
 	"半教師あり学習",
-	"転移学習",
-	"transfer learning",
-	"ドメイン適用",
-	"domain adaptation",
-	"data augmentation",
 	"事前学習",
 	"学習済みモデル",
 	"pretrain",
@@ -260,62 +372,21 @@ var keywords = []string{
 	"open source",
 	"オープンソース",
 	"python",
-	"julia",
 	"c++",
 	"tensorflow",
-	"tensorboard",
 	"keras",
 	"chainer",
 	"onnx",
 	"chainerrl",
 	"pytorch",
-	"allennlp",
 	"coreml",
-	"sagemaker",
-	"colab",
 	"numpy",
-	"scikit-learn",
-	"sklearn",
-	"pandas",
-	"scipy",
-	"hivemall",
-	"dask",
-	"nnabla",
-	"edward",
 	"jupyter",
-	"lightgbm",
-	"xgboost",
-	"ngboost",
 	"決定木",
 	"random forest",
 	"ランダムフォレスト",
 	"optuna",
-	"sentencepiece",
-	"lime",
-	"shap",
-	"openpose",
 	"pytext",
-	"scdv",
-	"tslearn",
-	"econml",
-	"deepgbm",
-	"sudachi",
-	"fairseq",
-	"tensor2tensor",
-	"stacknet",
-	"fsgan",
-	"deepfake",
-	"molgan",
-	"scibert",
-	"fastspeech",
-	"ncrfpp",
-	"blazeface",
-	"mecab",
-	"neo4j",
-	"leam",
-	"spacy",
-	"flair",
-	"continuous delivery",
 
 	"cnn",
 	"rnn",
@@ -324,39 +395,19 @@ var keywords = []string{
 	"オートエンコーダー",
 	"vae",
 	"bert",
-	"albert",
-	"elmo",
-	"electra",
 	"gan",
 	"generative adversarial network",
 	"敵対的生成ネットワーク",
 	"generative adversarial training",
 	"adversarial example",
-	"u-net",
 	"transformer",
-	"xlnet",
-	"stylegan",
-	"octave",
-	"gcn",
-	"cyclegan",
-	"mobilenet",
-	"noise2void",
-	"snorkel",
-	"yolact",
-	"kelpnet",
 	"vgg",
 	"adam",
 	"early stopping",
 	"分散学習",
 	"勾配法",
-	"hsic",
 	"biggan",
 	"bertsum",
-	"irgan",
-	"dialogpt",
-	"tinyvideonet",
-	"blazeface",
-	"singan",
 
 	"埋め込み",
 	"分散表現",
@@ -374,45 +425,20 @@ var keywords = []string{
 	"docker",
 	"コンテナ",
 	"kubernetes",
-	"kubeflow",
-	"sagemaker",
-	"s3",
-	"kafka",
-	"kinesis",
-	"elasticsearch",
-	"amazon forecast",
-	"splunk",
-	"mlops",
-	"automl",
-	"機械学習工学",
-	"reproducibility",
-	"cookiecutter",
-	"datarobot",
-	"dataflow",
-	"mlflow",
 
 	"kaggle",
-	"kaggle days",
-	"kaggle days tokyo",
 	"前処理",
 	"signate",
 	"コンペ",
 	"competition",
 	"bagging",
-	"abテスト",
-	"検定",
-	"statistics",
-	"統計学",
 
 	"self driving",
 	"自動運転",
 	"robotics",
-
-	"音声変換",
-	"link prediction",
 }
 
-func Init() error {
+func build(machine *goahocorasick.Machine, keywords []string) error {
 	var err error
 	once.Do(func() {
 		machine = new(goahocorasick.Machine)
@@ -429,13 +455,37 @@ func Init() error {
 		return err
 	}
 	return nil
+
 }
 
-func SearchKeywords(content string) []string {
+func Init() error {
+	if err := build(machinePriorityHigh, keywordsPriorityHigh); err != nil {
+		return err
+	}
+	if err := build(machinePriorityMedium, keywordsPriorityMedium); err != nil {
+		return err
+	}
+	if err := build(machinePriorityLow, keywordsPriorityLow); err != nil {
+		return err
+	}
+	return nil
+}
+
+func searchKeywords(machine *goahocorasick.Machine, content string) []string {
 	terms := machine.MultiPatternSearch([]rune(content), false)
 	foundKeywords := make([]string, 0)
 	for _, t := range terms {
 		foundKeywords = append(foundKeywords, string(t.Word))
 	}
 	return util.RemoveDuplicate(foundKeywords)
+}
+
+func SearchKeywords(content string) []string {
+	if terms := searchKeywords(machinePriorityHigh, content); len(terms) != 0 {
+		return terms
+	}
+	if terms := searchKeywords(machinePriorityMedium, content); len(terms) != 0 {
+		return terms
+	}
+	return searchKeywords(machinePriorityLow, content)
 }
