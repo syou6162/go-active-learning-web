bce4a01af769f6d931c820de798fd06a956d8eba
Yasuhisa Yoshida <syou6162@gmail.com>
2020-08-30 23:40:23 +0900

    ボタンの数をテスト

--- components/AnnotateButtons.spec.ts
+++ components/AnnotateButtons.spec.ts
@@ -17,9 +17,12 @@ describe('AnnotateButtons', () => {
       },
     })
     expect(wrapper.exists()).toBeTruthy()
+    expect(wrapper.findAll('button').length).toBe(3)
+
     expect(wrapper.findAll('button').at(0).classes()).toContain('btn-outline-primary')
     expect(wrapper.findAll('button').at(1).classes()).toContain('btn-outline-danger')
     expect(wrapper.findAll('button').at(2).classes()).toContain('btn-secondary')
+
     expect(wrapper.element).toMatchSnapshot()
   })
 })
